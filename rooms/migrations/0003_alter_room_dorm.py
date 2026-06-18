995192f5f123c87c633481f3aa60a6bed3e578f3
marube2005 <emarube89@gmail.com>
2026-06-18 09:59:56 +0300

    Fix: Added a migration file to fix the null value error

--- rooms/migrations/0003_alter_room_dorm.py
+++ rooms/migrations/0003_alter_room_dorm.py
@@ -2,6 +2,30 @@
 from django.db import migrations, models
 import django.db.models.deletion
 
+def populate_null_dorms(apps, schema_editor):
+    Room = apps.get_model('rooms', 'Room')
+    Dorm = apps.get_model('rooms', 'Dorm')
+    
+    null_rooms = Room.objects.filter(dorm__isnull=True)
+    if null_rooms.exists():
+        # Get first existing dorm or create 'Kilimanjaro'
+        dorm = Dorm.objects.first()
+        if not dorm:
+            dorm, _ = Dorm.objects.get_or_create(
+                name='Kilimanjaro',
+                defaults={
+                    'number_of_rooms': 1,
+                    'room_capacity': 4
+                }
+            )
+        for room in null_rooms:
+            room.dorm = dorm
+            room.save()
+        # Sync the dorm's room count
+        dorm.number_of_rooms = Room.objects.filter(dorm=dorm).count()
+        dorm.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -9,6 +33,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(populate_null_dorms, reverse_code=migrations.RunPython.noop),
         migrations.AlterField(
             model_name='room',
             name='dorm',
@@ -19,3 +44,4 @@ class Migration(migrations.Migration):
             ),
         ),
     ]
+
