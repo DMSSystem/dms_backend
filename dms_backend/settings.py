cc419669346c60f0f561463e546b8c28df7afd35
Elvis Marube <147842970+marube2005@users.noreply.github.com>
2026-07-06 14:26:01 +0300

    Add allowed origin for CORS configuration

--- dms_backend/settings.py
+++ dms_backend/settings.py
@@ -122,6 +122,7 @@ SPECTACULAR_SETTINGS = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://dms-frontend-jet.vercel.app"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
